6809ff7b04fe333eb31a0930bb7becf31e00bc61
ErnestoGP <ErnestoGPerez91@yahoo.com>
2016-01-20 16:17:02 -0800

    adding setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+	name = 'mypackage',
+	packages = ['mypackage'],
+	version = '0.0',
+	description = 'this is something, I dont know',
+	author = 'Ernesto Perez',
+	author_email = 'ErnestoGPerez91@yahoo.com',
+	url = 'https://github.com/ErnestoGP/testrep',
+
+	keywords = ['test', 'thing', 'Idunno'],
+	classifiers = [],
+)
