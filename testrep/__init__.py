61306c57f9b37c9484a0f02d35ef4a5fb033f8da
ErnestoGP <ErnestoGPerez91@yahoo.com>
2016-01-29 16:50:38 -0800

    bugfixing __init__.py

--- testrep/__init__.py
+++ testrep/__init__.py
@@ -12,9 +12,9 @@ class vecstuff():
 		self.z = z
 
 	def normalvector(self):
-		unit = sqrt(pow(self.x)+pow(self.y)+pow(self.z))
-		return self.x/unit, self.y/unit, self.z/unit
+		unit = sqrt(pow(self.x,2)+pow(self.y,2)+pow(self.z,2))
+		return [self.x/unit, self.y/unit, self.z/unit]
 
 	def length(self):
-		return sqrt(pow(self.x)+pow(self.y)+pow(self.z))
+		return sqrt(pow(self.x,2)+pow(self.y,2)+pow(self.z,2))
 			
